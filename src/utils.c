c972dcb16cc40b90cfa480f0ec37c359f075ba78
Kirisummer <ediktrikoz@gmail.com>
2023-10-17 05:30:00 +0300

    Update utils
    Fix invalid memory zero-out on realloc.
    Remove min/max macros.

--- src/utils.c
+++ src/utils.c
@@ -9,7 +9,7 @@ bui_alloc_st bui_realloc(big_uint *bui, size_t new_alloc) {
     if (new_ptr) {
         if (bui->alloc < new_alloc) {
             const size_t new_blocks = new_alloc - bui->alloc;
-            memset(new_ptr + bui->alloc, 0, new_blocks);
+            memset(new_ptr + bui->alloc, 0, sizeof(bui_block) * new_blocks);
         }
         bui->blocks = new_ptr;
         bui->alloc = new_alloc;
--- src/utils.h
+++ src/utils.h
@@ -5,9 +5,6 @@
 
 #include <stddef.h>
 
-#define bui_min(lhs, rhs) ((lhs) < (rhs))? (lhs): (rhs)
-#define bui_max(lhs, rhs) ((lhs) > (rhs))? (lhs): (rhs)
-
 bui_alloc_st bui_realloc(big_uint *bui, size_t new_alloc);
 
 
